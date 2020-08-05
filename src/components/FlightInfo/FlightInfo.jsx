b9eaf4ad7bf75b0795d35fb66804bfb7c3164d5f
Nerea <nereaparicio@gmail.com>
2020-08-06 01:13:51 +0200

    Added Spinner to load data and start to show data

--- src/components/FlightInfo/FlightInfo.jsx
+++ src/components/FlightInfo/FlightInfo.jsx
@@ -1,11 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React, { useState, useEffect } from 'react';
+import { BpkExtraLargeSpinner, SPINNER_TYPES } from 'bpk-component-spinner';
 import LongArrowRightIconSm from 'bpk-component-icon/sm/long-arrow-right';
 import { withAlignment } from 'bpk-component-icon';
 import { lineHeightBase, iconSizeSm, colors } from 'bpk-tokens/tokens/base.es6';
 import BpkText from 'bpk-component-text';
-import { withButtonAlignment, withRtlSupport } from 'bpk-component-icon';
-import ArrowIcon from 'bpk-component-icon/sm/long-arrow-right';
 import BpkButton from 'bpk-component-button';
 import BpkCard from 'bpk-component-card';
 import BpkSmallFlightIcon from 'bpk-component-icon/sm/flight';
@@ -18,16 +17,17 @@ import {
 
 import STYLES from './FlightInfo.scss';
 
-const AlignedArrowIcon = withButtonAlignment(withRtlSupport(ArrowIcon));
-
 const getClassName = className => STYLES[className] || 'UNKNOWN';
 
 const AlignedArrow = withAlignment(
-  LongArrowRightIconSm, lineHeightBase, iconSizeSm
+  LongArrowRightIconSm,
+  lineHeightBase,
+  iconSizeSm,
 );
 
 const FlightInfo = () => {
   const [dataFlight, setDataFlight] = useState(null);
+  let content = '';
 
   useEffect(() => {
     // eslint-disable-next-line no-undef
@@ -42,85 +42,177 @@ const FlightInfo = () => {
       });
   }, [setDataFlight]);
 
-  return (
-
-
-    <BpkCard>
-      <BpkTable className={getClassName('info-table')}>
-        <BpkTableBody>
-          <BpkTableRow>
-            <BpkTableCell>
-              <BpkSmallFlightIcon className="abc-icon__flight" />
-              </BpkTableCell>
-            <BpkTableCell>
-              <div>
-                Time-depart
-              </div>
-              <div className={getClassName('info-grey')}>
-                airport
-                </div>
-            </BpkTableCell>
-            <BpkTableCell> <AlignedArrow fill={colors.colorSkyGrayTint03} className={getClassName('arrow')}/> </BpkTableCell>
-            <BpkTableCell>
-              <div>
-                Time-arrival
-              </div>
-              <div className={getClassName('info-grey')}>
-                airport
-              </div>
-            </BpkTableCell>
-            <BpkTableCell>
-              <div className={getClassName('info-grey')}>
-                Time
-              </div>
-              <div>
-                Direct?
-              </div>
-            </BpkTableCell>
-          </BpkTableRow>
-          <BpkTableRow>
-            <BpkTableCell>
-            <BpkSmallFlightIcon className="abc-icon__flight" />
-              </BpkTableCell>
-            <BpkTableCell>
-              <div>
-                Time-depart
-              </div>
-              <div className={getClassName('info-grey')}>
-                airport
-              </div>
-            </BpkTableCell>
-            <BpkTableCell> <AlignedArrow fill={colors.colorSkyGrayTint03} className={getClassName('arrow')}/> </BpkTableCell>
-            <BpkTableCell>
-              <div>
-                Time-arrival
-                </div>
-              <div className={getClassName('info-grey')}>
-                airport
-                </div>
-            </BpkTableCell>
-            <BpkTableCell>
-              <div className={getClassName('info-grey')}>
-                Time
-                </div>
-              <div>
-                Direct?
-                </div>
-            </BpkTableCell>
-          </BpkTableRow>
-        </BpkTableBody>
-      </BpkTable>
-      <div className={getClassName('card-footer-row')}>
-        <div>
-          <BpkText>
-            Price
-            <BpkText className={getClassName('info-grey')}><br />pagweb</BpkText>
-          </BpkText>
-        </div>
-        <BpkButton large>Select</BpkButton>
+  if (dataFlight !== null && dataFlight.itineraries.length > 0) {
+    content = dataFlight.itineraries.map((itinerary, idx) => {
+      return (
+        <BpkCard className={getClassName('card')} key={idx}>
+          <BpkTable className={getClassName('info-table')}>
+            <BpkTableBody>
+              <BpkTableRow>
+                <BpkTableCell>
+                  <BpkSmallFlightIcon className="abc-icon__flight" />
+                </BpkTableCell>
+                <BpkTableCell>
+                  <div>
+                    Time-depart
+                  </div>
+                  <div className={getClassName('info-grey')}>
+                    airport
+                  </div>
+                </BpkTableCell>
+                <BpkTableCell> <AlignedArrow fill={colors.colorSkyGrayTint03} className={getClassName('arrow')} /> </BpkTableCell>
+                <BpkTableCell>
+                  <div>
+                    Time-arrival
+                  </div>
+                  <div className={getClassName('info-grey')}>
+                    airport
+                  </div>
+                </BpkTableCell>
+                <BpkTableCell>
+                  <div className={getClassName('info-grey')}>
+                    Time
+                  </div>
+                  <div>
+                    Direct?
+                  </div>
+                </BpkTableCell>
+              </BpkTableRow>
+              <BpkTableRow>
+                <BpkTableCell>
+                  <BpkSmallFlightIcon className="abc-icon__flight" />
+                </BpkTableCell>
+                <BpkTableCell>
+                  <div>
+                    Time-depart
+                  </div>
+                  <div className={getClassName('info-grey')}>
+                    airport
+                  </div>
+                </BpkTableCell>
+                <BpkTableCell> <AlignedArrow fill={colors.colorSkyGrayTint03} className={getClassName('arrow')} /> </BpkTableCell>
+                <BpkTableCell>
+                  <div>
+                    Time-arrival
+                  </div>
+                  <div className={getClassName('info-grey')}>
+                    airport
+                  </div>
+                </BpkTableCell>
+                <BpkTableCell>
+                  <div className={getClassName('info-grey')}>
+                    Time
+                  </div>
+                  <div>
+                    Direct?
+                  </div>
+                </BpkTableCell>
+              </BpkTableRow>
+            </BpkTableBody>
+          </BpkTable>
+          <div className={getClassName('card-footer-row')}>
+            <div>
+              <BpkText className={getClassName('itinerary-price')}>
+                {itinerary.price}
+                <BpkText className={getClassName('info-grey-light')}>
+                  <br />
+                  {itinerary.agent}
+                </BpkText>
+              </BpkText>
+            </div>
+            <BpkButton>Select</BpkButton>
+          </div>
+        </BpkCard>
+      )
+    })
+    return (
+      content
+    )
+    //   return (
+    //   <BpkCard>
+    //     <BpkTable className={getClassName('info-table')}>
+    //       <BpkTableBody>
+    //         <BpkTableRow>
+    //           <BpkTableCell>
+    //             <BpkSmallFlightIcon className="abc-icon__flight" />
+    //           </BpkTableCell>
+    //           <BpkTableCell>
+    //             <div>
+    //               Time-depart
+    //             </div>
+    //             <div className={getClassName('info-grey')}>
+    //               airport
+    //               </div>
+    //           </BpkTableCell>
+    //           <BpkTableCell> <AlignedArrow fill={colors.colorSkyGrayTint03} className={getClassName('arrow')} /> </BpkTableCell>
+    //           <BpkTableCell>
+    //             <div>
+    //               Time-arrival
+    //             </div>
+    //             <div className={getClassName('info-grey')}>
+    //               airport
+    //             </div>
+    //           </BpkTableCell>
+    //           <BpkTableCell>
+    //             <div className={getClassName('info-grey')}>
+    //               Time
+    //             </div>
+    //             <div>
+    //               Direct?
+    //             </div>
+    //           </BpkTableCell>
+    //         </BpkTableRow>
+    //         <BpkTableRow>
+    //           <BpkTableCell>
+    //             <BpkSmallFlightIcon className="abc-icon__flight" />
+    //           </BpkTableCell>
+    //           <BpkTableCell>
+    //             <div>
+    //               Time-depart
+    //             </div>
+    //             <div className={getClassName('info-grey')}>
+    //               airport
+    //             </div>
+    //           </BpkTableCell>
+    //           <BpkTableCell> <AlignedArrow fill={colors.colorSkyGrayTint03} className={getClassName('arrow')} /> </BpkTableCell>
+    //           <BpkTableCell>
+    //             <div>
+    //               Time-arrival
+    //               </div>
+    //             <div className={getClassName('info-grey')}>
+    //               airport
+    //               </div>
+    //           </BpkTableCell>
+    //           <BpkTableCell>
+    //             <div className={getClassName('info-grey')}>
+    //               Time
+    //               </div>
+    //             <div>
+    //               Direct?
+    //               </div>
+    //           </BpkTableCell>
+    //         </BpkTableRow>
+    //       </BpkTableBody>
+    //     </BpkTable>
+    //     <div className={getClassName('card-footer-row')}>
+    //       <div>
+    //         <BpkText>
+    //           itinerary.price
+    //           <BpkText className={getClassName('info-grey')}><br />itinerary.agent</BpkText>
+    //         </BpkText>
+    //       </div>
+    //       <BpkButton large>Select</BpkButton>
+    //     </div>
+    //   </BpkCard>
+    // )
+    // eslint-disable-next-line no-else-return
+  } else {
+    return (
+      <div className={getClassName('spinner')}>
+        <BpkExtraLargeSpinner type={SPINNER_TYPES.primary} />
       </div>
-    </BpkCard>
-  )
+    );
+  }
 };
 
 export default FlightInfo;
